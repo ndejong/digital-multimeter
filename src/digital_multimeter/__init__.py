e67654f0928e35ac6a22986dae6e0424c3f608e0
Nicholas de Jong <me@nicholasdejong.com>
2020-12-05 11:01:51 +1000

    Bumps to version 0.2.0 to roll up recent changes

--- src/digital_multimeter/__init__.py
+++ src/digital_multimeter/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2020 Nicholas de Jong
 
 __author__ = 'Nicholas de Jong <contact@nicholasdejong.com>'
-__version__ = '0.1.6'
+__version__ = '0.2.0'
 __title__ = "digital-multimeter"
 __license__ = "MIT"
 
---
+++ src/test/test_click_actions.py
@@ -0,0 +1,29 @@
+
+import pytest
+from click.testing import CliRunner
+from digital_multimeter.cli import click
+from digital_multimeter import __version__
+
+
+def test_dmm_version():
+    runner = CliRunner()
+    result = runner.invoke(click.dmm, '--version')
+    assert __version__ in result.output
+    assert result.exit_code == 0
+
+
+def test_dmm_help():
+    runner = CliRunner()
+    result = runner.invoke(click.dmm, '--help')
+    assert 'Usage:' in result.output
+    assert 'Options:' in result.output
+    assert 'Commands:' in result.output
+    assert result.exit_code == 0
+
+
+def test_get_models_supported():
+    runner = CliRunner()
+    result = runner.invoke(click.get_models_supported)
+    assert 'models' in result.output
+    assert 'Default' in result.output
+    assert result.exit_code == 0
